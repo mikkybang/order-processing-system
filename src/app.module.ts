8834744527421947f2c060869c60c6dbcbcbe91a
Michael Efemena <mefemena@gmail.com>
2024-01-15 22:07:53 +0100

    generate order-log resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { OrderLogModule } from './order-log/order-log.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [OrderModule],
+  imports: [OrderModule, OrderLogModule],
   controllers: [AppController],
   providers: [AppService],
 })
