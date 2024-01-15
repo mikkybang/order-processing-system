498936c6bd48f99b24010f61792675e30894571c
Michael Efemena <mefemena@gmail.com>
2024-01-15 22:11:01 +0100

    generate order-type resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { OrderTypeModule } from './order-type/order-type.module';
 import { BrandModule } from './brand/brand.module';
 import { AddonModule } from './addon/addon.module';
 import { MealModule } from './meal/meal.module';
@@ -10,7 +11,7 @@ import { OrderLogModule } from './order-log/order-log.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [OrderModule, OrderLogModule, CalculatedOrderModule, MealModule, AddonModule, BrandModule],
+  imports: [OrderModule, OrderLogModule, CalculatedOrderModule, MealModule, AddonModule, BrandModule, OrderTypeModule],
   controllers: [AppController],
   providers: [AppService],
 })
