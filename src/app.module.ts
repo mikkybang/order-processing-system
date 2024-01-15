9b1e4e69726ea7427ac7b192211ace0208270872
Michael Efemena <mefemena@gmail.com>
2024-01-15 22:10:33 +0100

    generate brand resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { BrandModule } from './brand/brand.module';
 import { AddonModule } from './addon/addon.module';
 import { MealModule } from './meal/meal.module';
 import { CalculatedOrderModule } from './calculated-order/calculated-order.module';
@@ -9,7 +10,7 @@ import { OrderLogModule } from './order-log/order-log.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [OrderModule, OrderLogModule, CalculatedOrderModule, MealModule, AddonModule],
+  imports: [OrderModule, OrderLogModule, CalculatedOrderModule, MealModule, AddonModule, BrandModule],
   controllers: [AppController],
   providers: [AppService],
 })
