b45d4fb6b330323867d53a9aaee140f7a7012733
Michael Efemena <mefemena@gmail.com>
2024-01-15 22:09:13 +0100

    generate meal resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { MealModule } from './meal/meal.module';
 import { CalculatedOrderModule } from './calculated-order/calculated-order.module';
 import { OrderLogModule } from './order-log/order-log.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [OrderModule, OrderLogModule, CalculatedOrderModule],
+  imports: [OrderModule, OrderLogModule, CalculatedOrderModule, MealModule],
   controllers: [AppController],
   providers: [AppService],
 })
