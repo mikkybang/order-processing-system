d57d5c6b70574172b49342dd965a557a3698bf3e
Michael Efemena <mefemena@gmail.com>
2024-01-16 00:00:02 +0100

    add knexfile and knex commands to package.json

---
+++ src/knexfile.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { knexSnakeCaseMappers } from 'objection';
+
+module.exports = {
+  client: 'pg',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USERNAME,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: Boolean(process.env.DATABASE_SSL)
+      ? { rejectUnauthorized: false }
+      : false,
+  },
+  migrations: {
+    directory: './src/database/migrations',
+    stub: './src/database/migration.stub',
+  },
+  seeds: {
+    directory: './src/database/seeds',
+    stub: './src/database/seed.stub',
+  },
+  ...knexSnakeCaseMappers(),
+};
