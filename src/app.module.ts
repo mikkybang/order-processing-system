1eada4ed2ad6618c41a1f72915e066b62025948d
Michael Efemena <mefemena@gmail.com>
2024-01-17 22:47:49 +0100

    add GlobalMiddleware to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderTypeModule } from './order-type/order-type.module';
@@ -10,6 +10,7 @@ import { OrderLogModule } from './order-log/order-log.module';
 import { OrderModule } from './order/order.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { LoggerMiddleware } from './common/middleware/GlobalLoggerMiddleware';
 
 @Module({
   imports: [
@@ -28,4 +29,9 @@ import configuration from './config/configuration';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
