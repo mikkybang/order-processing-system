cc69c692386caf12b8ff96d07a43c9ea89e2143b
Michael Efemena <mefemena@gmail.com>
2024-01-17 03:12:43 +0100

    fill meals migration columns

--- src/database/migrations/20240117020001_Meals.ts
+++ src/database/migrations/20240117020001_Meals.ts
@@ -6,7 +6,12 @@ export async function up(knex: Knex) {
   return knex.schema.createTable(tableName, (table) => {
     // this creates an "id" column that gets autoincremented
     table.increments();
-
+    table.string('name').notNullable();
+    table.string('brand_id').references('id').inTable('brands');
+    table.boolean('active').notNullable();
+    table.decimal('amount', 14, 2);
+    table.jsonb('images');
+    table.string('item_type');
     table
       .dateTime('created_at')
       .notNullable()
