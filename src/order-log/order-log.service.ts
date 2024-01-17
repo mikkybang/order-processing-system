d653e98ff8ffeb1a24eed78c634c4ce36ce833ab
Michael Efemena <mefemena@gmail.com>
2024-01-17 23:48:43 +0100

    Add CRUD functions to order log service

--- src/order-log/order-log.service.ts
+++ src/order-log/order-log.service.ts
@@ -1,26 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrderLogDto } from './dto/create-order-log.dto';
 import { UpdateOrderLogDto } from './dto/update-order-log.dto';
+import { OrderLogModel } from './models/order-log.model';
+import { ModelClass } from 'objection';
 
 @Injectable()
 export class OrderLogService {
+  constructor(
+    @Inject('OrderLogModel') private readonly orderLogModel: ModelClass<OrderLogModel>
+  ) { }
   create(createOrderLogDto: CreateOrderLogDto) {
-    return 'This action adds a new orderLog';
+    return this.orderLogModel.query().insert(createOrderLogDto).returning('*');
   }
 
   findAll() {
-    return `This action returns all orderLog`;
+    return this.orderLogModel.query();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} orderLog`;
+    return this.orderLogModel.query().findById(id);
   }
 
   update(id: number, updateOrderLogDto: UpdateOrderLogDto) {
-    return `This action updates a #${id} orderLog`;
+    return this.orderLogModel.query().patchAndFetchById(id, updateOrderLogDto).returning('*');
   }
 
   remove(id: number) {
-    return `This action removes a #${id} orderLog`;
+    return this.orderLogModel.query().deleteById(id).returning('*');
   }
 }
