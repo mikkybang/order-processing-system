2283cf418562b14cee019dd3855fdc113c75ff1a
Michael Efemena <mefemena@gmail.com>
2024-01-17 03:11:17 +0100

    fill calculated_orders migration columns

--- src/database/migrations/20240117015953_CalculatedOrders.ts
+++ src/database/migrations/20240117015953_CalculatedOrders.ts
@@ -7,6 +7,19 @@ export async function up(knex: Knex) {
     // this creates an "id" column that gets autoincremented
     table.increments();
 
+    table.decimal('total_amount', 14, 2).notNullable();
+    table.boolean('free_delivery').notNullable();
+    table.decimal('delivery_fee', 14, 2).notNullable();
+    table.decimal('service_charge', 14, 2).notNullable();
+    table.jsonb('address_details').notNullable();
+    table.jsonb('meals').notNullable();
+    table.decimal('lat', 9, 6);
+    table.decimal('lng', 9, 6);
+    table.string('cokitchen_polygon_id');
+    table.string('user_id');
+    table.string('cokitchen_id');
+    table.boolean('pickup').notNullable();
+    table.decimal('prev_price', 14, 2);
     table
       .dateTime('created_at')
       .notNullable()
