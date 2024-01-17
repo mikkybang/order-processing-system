e9cfe50eba6906420c8e4a348f358670c169edfa
Michael Efemena <mefemena@gmail.com>
2024-01-17 03:15:12 +0100

    fill addonss migration columns

--- src/database/migrations/20240117020007_Addons.ts
+++ src/database/migrations/20240117020007_Addons.ts
@@ -6,7 +6,9 @@ export async function up(knex: Knex) {
   return knex.schema.createTable(tableName, (table) => {
     // this creates an "id" column that gets autoincremented
     table.increments();
-
+    table.decimal('amount', 14, 2).notNullable();
+    table.string('meal_id').references('id').inTable('meals'); // Foreign key to the Meal table
+    table.jsonb('meal_data');
     table
       .dateTime('created_at')
       .notNullable()
