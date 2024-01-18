ce4c1f5768880663c6a5aeb2eed12a7a3781718e
Michael Efemena <mefemena@gmail.com>
2024-01-18 02:52:08 +0100

    add relationships ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssadd brand and addon relationship to meal

--- src/meal/models/meal.model.ts
+++ src/meal/models/meal.model.ts
@@ -2,6 +2,7 @@
 import { BrandModel } from 'src/brand/models/brand.model';
 import { BaseModel } from '../../database/models/base.model';
 import { Model } from 'objection';
+import { AddonModel } from 'src/addon/models/addon.model';
 
 export class MealModel extends BaseModel {
   static tableName = 'meals';
@@ -14,6 +15,9 @@ export class MealModel extends BaseModel {
   images: Record<string, any>;
   itemType: string;
 
+  brand: BrandModel;
+  addons: AddonModel[];
+
   static relationMappings = {
     brand: {
       relation: Model.BelongsToOneRelation,
@@ -21,6 +25,14 @@ export class MealModel extends BaseModel {
       join: {
         from: 'meals.brand_id',
         to: 'brands.id'
+      }
+    },
+    addons: {
+      relation: Model.HasManyRelation,
+      modelClass: AddonModel, // Adjust the path as needed
+      join: {
+        from: 'meals.id',
+        to: 'addons.meal_id'
       }
     }
   };
